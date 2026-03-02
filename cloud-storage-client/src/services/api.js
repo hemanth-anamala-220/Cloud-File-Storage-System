283654aa748f57e03d79d61d1503ea77d7d4eac6
hemanth-anamala-220 <hemanthanamala220@gmail.com>
2026-03-02 13:43:15 +0530

    Update api.js

--- cloud-storage-client/src/services/api.js
+++ cloud-storage-client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://cloud-file-storage-system-1-backend.onrender.com/api',
 })
 
 // Attach JWT token to every request
